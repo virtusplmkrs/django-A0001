70eaeee4058177b0e24a2897a19f91d1170de097
Alexandre Yuri <virtusr.plmkrs@gmail.com>
2022-10-21 18:31:59 -0300

    commit final

--- djangotestprjt/djangotestapp/views.py
+++ djangotestprjt/djangotestapp/views.py
@@ -1,12 +1,10 @@
-from pyexpat import model
-from re import template
-from django.shortcuts import generic
+from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-  queryset = Post.objects.filter(status=1).order_by('-created_on')
-  template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
 
 class PostDetail(generic.DetailView):
-  model = Post
-  template_name = 'post_detail.html'
+    model = Post
+    template_name = 'post_detail.html'
